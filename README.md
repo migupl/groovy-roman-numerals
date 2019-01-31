b4f6637169383461eec915cf9dfaba5cdad87ecb
Miguel Pagola Lorenzo <mipagola@gmail.com>
2019-01-31 17:54:00 +0100

    Run as script

--- README.md
+++ README.md
@@ -2,4 +2,21 @@
 
 Roman Numerals Subtractive Notation implementation ([Basic decimal pattern][1])
 
+Use
+```bash
+$ ./gradlew -q
+
+Use: ./gradlew --number=<number>  // Number in 1 .. 3000
+
+```
+for getting help
+
+Example,
+```bash
+$ ./gradlew -q -Pnumber=2019
+
+'MMXIX' is Roman Numeral for 2019
+
+```
+
 [1]: https://en.wikipedia.org/wiki/Roman_numerals#Basic_decimal_pattern
---
+++ src/main/groovy/CalcRomanNumeral.groovy
@@ -0,0 +1,29 @@
+import com.roman.service.RomanNumeralsService
+
+if (args) {
+    try {
+        def (int number, service) = [
+                args[0].toInteger(),
+                RomanNumeralsService.instance
+        ]
+
+        def romanNumeral = service.valueToSubtractiveNotation(number)
+
+        println """
+'$romanNumeral' is Roman Numeral for $number
+"""
+
+    } catch (NumberFormatException _) {
+        helpMessage()
+
+    }
+
+} else {
+    helpMessage()
+}
+
+def helpMessage() {
+    println """
+Use: ./gradlew --number=<number>  // Number in 1 .. 3000
+"""
+}
